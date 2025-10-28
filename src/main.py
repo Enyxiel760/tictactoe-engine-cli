ce4e196eb4ed21593dac2ab64af2321d3fdec613
Enyxiel760 <enyxiel760@gmail.com>
2025-10-28 17:00:59 +0000

    Initial commit: setup project

---
+++ src/main.py
@@ -0,0 +1,31 @@
+board = new_board()
+
+#loop through turns until game over
+loop forever:
+    #TODO
+    current_player = ???
+
+    #print trhe current borad state
+    render(board)
+
+    #get the move from current player
+    move = get_move()
+
+    #make the move on the board
+    make_move(board, current_player, move)
+
+    #work out if theirs a winner
+    winner = get_winner(board)
+
+    #if there is a winner, exit loop
+    if winner is not none:
+        print "winner is" winner
+        break
+
+    #if no winner and board full
+    if is_board_full(board):
+        print "draw"
+        break
+
+#repeat until game over
+    
